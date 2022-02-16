181de54fa64b65bbdfb3d4973b04de73fc49ee9b
Mattia Barbanera <mattia.barbanera@pg.infn.it>
2022-02-16 14:55:01 +0100

    add axiFifo class for FIFOs; rename hpsServer

---
+++ include/axiFifo.h
@@ -0,0 +1,93 @@
+#ifndef AXIFIFO_H
+#define AXIFIFO_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <string.h>
+
+/*!
+  AXI FIFO properties and methods
+  
+  The pointer arithmetics below relies on the type (void*, char*, int*, etc...) of the pointers:
+    - if the pointer is void* summing 1 (pointer + 1) means actually summing 4 (since the compiler knows that you have to move of 4 bytes)
+    - if the pointer is char* summing 1 (pointer + 1) means actually summing 1 (since the compiler knows that you have to move of 1 bytes)
+  This arithmetics must be compatible with the offset defined
+*/
+class axiFifo {
+  protected:
+    uint32_t* addr; //!< Base address (corresponds to data or q)
+    uint32_t* CsrAddr; //!< CSR address
+    uint32_t* UsedwAddr; //!< Used-words address
+    uint32_t* StatusAddr; //!< Status address
+    
+    uint32_t* AFullReg; //!< Almost-full threshold register
+    uint32_t* AEmptyReg; //!< Almost-empty threshold register
+    uint32_t* EventReg; //!< Events register
+    uint32_t* iEnableReg; //!< Interrupt-Enable register
+
+  public:
+    axiFifo(void* virtualBase, uint32_t address, uint32_t csr, uint32_t aEmptyThr, uint32_t aFullThr, uint8_t interruptEn);
+
+    //!< Return the number of used words
+    inline uint32_t getUsedw ();
+
+    //!< Return the full flag
+    inline bool getFull ();
+
+    //!< Return the almost-full flag
+    inline bool getAFull ();
+
+    //!< Return the empty flag
+    inline bool getEmpty ();
+
+    //!< Return the almost-empty flag
+    inline bool getAEmpty ();
+
+    //!< Return the overflow flag
+    inline bool getOverFlow ();
+
+    //!< Reset overflow flag
+    inline void resetOverflow ();
+
+    /*!
+      FIFO initialization: Set aFull and aEmpty thresholds, disable interrupts, and reset events register
+      @param aEmptyThr Almost-empty threshold in number of words
+      @param aFullThr Almost-full threshold in number of words
+      @param interruptEn Interrupt enable as required by Intel
+    */
+    void init (uint32_t aEmptyThr, uint32_t aFullThr, uint8_t interruptEn);
+
+    /*!
+      Write single word
+      @param data Data to be written
+      @return -1 if error, number of written bytes
+    */
+    int write(uint32_t* data);
+
+    /*!
+      Write a chunk of data of length len
+      @param data Data to be written
+      @param len Amount of bytes to be written
+      @return -1 if full, number of written bytes
+    */
+    int writeChunck(uint32_t* data, int len);
+
+    /*!
+      Read single word
+      @param data Memory where to write
+      @return -1 if empty, number of read bytes
+    */
+    int read(uint32_t* data);
+
+    /*!
+      Read a chunk of data of length len
+      @param data Memory where to write
+      @param len Amount of bytes to read
+      @return -1 if words are not enough, number of read bytes
+    */
+    int readChunk(uint32_t* data, int lenChunk, bool flush);
+};
+
+#endif
---
+++ src/axiFifo.cpp
@@ -0,0 +1,118 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "hps_0.h"
+#include "user_avalon_fifo_regs.h"
+#include "server.h"
+#include "utility.h"
+
+#include "axiFifo.h"
+
+axiFifo::axiFifo(void* virtualBase, uint32_t address, uint32_t csr, uint32_t aEmptyThr, uint32_t aFullThr, uint8_t interruptEn){
+  //Base Data and CST addresses: the shifts are in units of bytes
+  addr = (uint32_t*)((unsigned long)virtualBase + ((unsigned long)(ALT_LWFPGASLVS_OFST + address) & (unsigned long)(HW_REGS_MASK)));
+  CsrAddr = (uint32_t*)((unsigned long)virtualBase + ((unsigned long)(ALT_LWFPGASLVS_OFST + csr) & (unsigned long)(HW_REGS_MASK)));
+  // The shifts are in units of 4-bytes
+  UsedwAddr = CsrAddr + (unsigned long)ALTERA_AVALON_FIFO_LEVEL_REG;
+  StatusAddr = CsrAddr + (unsigned long)ALTERA_AVALON_FIFO_STATUS_REG;
+
+  AFullReg = CsrAddr + (unsigned long)ALTERA_AVALON_FIFO_ALMOSTFULL_REG;
+  AEmptyReg = CsrAddr + (unsigned long)ALTERA_AVALON_FIFO_ALMOSTEMPTY_REG;
+  EventReg = CsrAddr + (unsigned long)ALTERA_AVALON_FIFO_EVENT_REG;
+  iEnableReg = CsrAddr + (unsigned long)ALTERA_AVALON_FIFO_IENABLE_REG;
+
+  init(aEmptyThr, aFullThr, interruptEn);
+}
+
+inline uint32_t axiFifo::getUsedw () {
+  return *UsedwAddr;
+};
+
+inline bool axiFifo::getFull () {
+ return (*StatusAddr & ALTERA_AVALON_FIFO_STATUS_F_MSK) && 1;
+};
+
+inline bool axiFifo::getAFull () {
+	return (*StatusAddr & ALTERA_AVALON_FIFO_STATUS_AF_MSK) && 1;
+};
+
+inline bool axiFifo::getEmpty () {
+	return (*StatusAddr & ALTERA_AVALON_FIFO_STATUS_E_MSK) && 1;
+};
+
+inline bool axiFifo::getAEmpty () {
+	return (*StatusAddr & ALTERA_AVALON_FIFO_STATUS_AE_MSK) && 1;
+};
+
+inline bool axiFifo::getOverFlow () {
+	return (*EventReg & ALTERA_AVALON_FIFO_EVENT_OVF_MSK) && 1;
+};
+
+inline void axiFifo::resetOverflow () {
+  *EventReg &= ALTERA_AVALON_FIFO_EVENT_OVF_MSK;
+};
+
+void axiFifo::init (uint32_t aEmptyThr, uint32_t aFullThr, uint8_t interruptEn) {
+  *EventReg		= ALTERA_AVALON_FIFO_EVENT_ALL_MSK;
+  *AEmptyReg	= aEmptyThr;
+  *AFullReg	  = aFullThr;
+  *iEnableReg = interruptEn & ALTERA_AVALON_FIFO_IENABLE_ALL_MSK;
+  return;
+};
+
+int axiFifo::write(uint32_t* data){
+  if (getFull()) {
+    return -1;
+  }
+  *addr = *data;
+  return 4;
+};
+
+int axiFifo::writeChunck(uint32_t* data, int len){
+  if (getFull()) {//FIXME: Should check the A-Full before writing
+    return -1;
+  }
+  
+  for (int i=0; i<len; i++){
+    *addr = data[i];
+  }
+  return len;
+};
+
+int axiFifo::read(uint32_t* data){
+  if (getEmpty()) {
+    return -1;
+  }
+  *data = *addr;
+  return 4;
+};
+
+int axiFifo::readChunk(uint32_t* data, int lenChunk, bool flush){
+  int len = lenChunk;
+  uint32_t actualLevel = getUsedw();
+
+  if (flush == true) {
+    len = (uint32_t)actualLevel;
+  }
+
+  if ((uint32_t)len > actualLevel) {
+    //if(baseAddr.verbose > 3){
+    //  printf("%s) Tried to read %d, while fifo level was %d\n", __METHOD_NAME__, _len, (int)(actualLevel));
+    //  uint32_t regContent;
+    //  ReadReg(21, &regContent);
+    //  printf("Register 21: %08x\n", regContent);
+    //  ReadReg(22, &regContent);
+    //  printf("Register 22: %08x\n", regContent);
+    //}
+    return (-1);
+  } else {
+    for (int i=0; i<len; i++){
+      data[i] = *addr;
+    }
+  };
+  return len;
+  
+};
